ad7a0463b3930a04aacc125ee403bb2729339478
Tung Nguyen <tongueroo@gmail.com>
2020-11-11 14:44:31 +0000

    config.base64 option

--- lib/kubes_google/config.rb
+++ lib/kubes_google/config.rb
@@ -4,9 +4,13 @@ module KubesGoogle
 
     def defaults
       c = ActiveSupport::OrderedOptions.new
+      c.base64_secrets = true
       c.gke = ActiveSupport::OrderedOptions.new
       c.gke.cluster_name = nil
+      c.gke.enable_get_credentials = nil
       c.gke.enable_hooks = nil # nil since need cluster_name also. setting to false will explicitly disable hooks
+      c.gke.google_project = nil
+      c.gke.google_region = nil
       c.gke.whitelist_ip = nil # default will auto-detect IP
       c
     end
