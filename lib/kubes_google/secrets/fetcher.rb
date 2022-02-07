0b3e5fd3ce9c98e5cc70c6a7837dcf94b5518660
Tung Nguyen <tongueroo@gmail.com>
2022-02-07 00:30:39 +0000

    performance improvement: cache secrets

--- lib/kubes_google/secrets/fetcher.rb
+++ lib/kubes_google/secrets/fetcher.rb
@@ -1,13 +1,17 @@
 class KubesGoogle::Secrets
   class Fetcher
+    include KubesGoogle::Logging
     extend Memoist
 
     def initialize(options={})
       @options = options
     end
 
+    @@cache = {}
     def fetch(short_name)
-      fetcher.fetch(short_name)
+      return @@cache[short_name] if @@cache[short_name]
+      logger.debug "Fetching secret: #{short_name}"
+      @@cache[short_name] = fetcher.fetch(short_name)
     rescue KubesGoogle::VpnSslError
       logger.info "Retry fetching secret with the gcloud strategy"
       fetcher = Gcloud.new(@options)
